082bcc84ee8c2dc4fa6e43a2af0cc66f13897bb0
Jari Koopman <j.koopman@jarict.nl>
2018-08-10 14:17:31 +0200

    Start on tests

--- tests.py
+++ tests.py
@@ -1,15 +1,18 @@
 from TodoAPI.app import app
 from TodoAPI.models import Todo
+from TodoAPI.sql import init_db
+import sys
 
 from unittest import TestCase, main as execute_tests
 
 class TestAPI(TestCase):
-    def setup(self):
+    def setUp(self):
+        init_db()
         self.client = app.test_client()
 
     def test_index(self):
         r = self.client.get('/api/v1/todos')
-        print(r.json)
+        print(r.status_code)
 
 if __name__ == '__main__':
     execute_tests()
